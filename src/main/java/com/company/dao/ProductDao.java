6e4e857c1ef4671751892ea8033213a0becd93a3
Bartosz Maleta <bartosz.maleta@gmail.com>
2020-09-11 01:50:04 +0200

    Add needed methods

--- src/main/java/com/company/dao/ProductDao.java
+++ src/main/java/com/company/dao/ProductDao.java
@@ -1,4 +1,10 @@
 package com.company.dao;
 
+import com.company.model.Product;
+
 public interface ProductDao {
+    Product[] getProductTypes();
+    double getProductCost();
+    int getProductInventory(Product productType);
+    void setProductInventory(int productInventory);
 }
