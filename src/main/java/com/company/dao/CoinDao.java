1ef996059540e29a31db70a5a2aa16e300e16662
Bartosz Maleta <bartosz.maleta@gmail.com>
2020-09-11 01:50:25 +0200

    Add needed methods

--- src/main/java/com/company/dao/CoinDao.java
+++ src/main/java/com/company/dao/CoinDao.java
@@ -1,4 +1,10 @@
 package com.company.dao;
 
+import com.company.model.Coin;
+
 public interface CoinDao {
+    Coin[] getCoinTypes();
+    double getCoinValue();
+    int getCoinInventory(Coin cointype);
+    void setCoinInventory(int coinInv);
 }
