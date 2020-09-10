de0d5955796d844c6521526bae6c61e4d8ae29fe
Bartosz Maleta <bartosz.maleta@gmail.com>
2020-09-11 01:53:22 +0200

    Add overrides methods

--- src/main/java/com/company/dao/CoinDaoImpl.java
+++ src/main/java/com/company/dao/CoinDaoImpl.java
@@ -1,4 +1,26 @@
 package com.company.dao;
 
-public class CoinDaoImpl {
+import com.company.model.Coin;
+
+public class CoinDaoImpl implements CoinDao {
+
+    @Override
+    public Coin[] getCoinTypes() {
+        return Coin.values();
+    }
+
+    @Override
+    public double getCoinValue() {
+        return this.getCoinValue();
+    }
+
+    @Override
+    public int getCoinInventory(Coin cointype) {
+        return this.getCoinInventory(cointype);
+    }
+
+    @Override
+    public void setCoinInventory(int coinInv) {
+        this.setCoinInventory(coinInv);
+    }
 }
