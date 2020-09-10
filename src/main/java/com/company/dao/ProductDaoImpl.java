4b391304e3ba8f2adb4e0c11f7a0f2f3e662d1e0
Bartosz Maleta <bartosz.maleta@gmail.com>
2020-09-11 01:52:51 +0200

    Add overrides methods

--- src/main/java/com/company/dao/ProductDaoImpl.java
+++ src/main/java/com/company/dao/ProductDaoImpl.java
@@ -1,4 +1,26 @@
 package com.company.dao;
 
-public class ProductDaoImpl {
+import com.company.model.Product;
+
+public class ProductDaoImpl implements ProductDao {
+
+    @Override
+    public Product[] getProductTypes() {
+        return Product.values();
+    }
+
+    @Override
+    public double getProductCost() {
+        return this.getProductCost();
+    }
+
+    @Override
+    public int getProductInventory(Product productType) {
+        return this.getProductInventory(productType);
+    }
+
+    @Override
+    public void setProductInventory(int productInventory) {
+        this.setProductInventory(productInventory);
+    }
 }
