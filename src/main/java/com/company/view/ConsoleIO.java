6846a151e2d0822d22dc51f760e11898d8f9da65
Bartosz Maleta <bartosz.maleta@gmail.com>
2020-09-11 01:51:00 +0200

    Add Terminal helpers methods

--- src/main/java/com/company/view/ConsoleIO.java
+++ src/main/java/com/company/view/ConsoleIO.java
@@ -1,4 +1,35 @@
 package com.company.view;
 
+import java.util.Scanner;
+
 public class ConsoleIO {
+    Scanner sc = new Scanner(System.in);
+
+    public int queryUserInt(String queryMessage){
+        System.out.println(queryMessage);
+        return sc.nextInt();
+    }
+
+    public int queryUserIntRange(String queryMessage, int inputMin, int inputMax){
+        System.out.println(queryMessage);
+        int response = sc.nextInt();
+        if (response < inputMin && response > inputMax) {
+            System.out.println("Looks like you are out of range, try again!");
+            System.out.println("Please stay within " + inputMin + " and " + inputMax + ".");
+            System.out.println(queryMessage);
+            response = sc.nextInt();
+        }
+        return response;
+    }
+
+    public String queryUserString(String queryMessage){
+        System.out.println(queryMessage);
+        String resp = sc.next();
+        return resp;
+    }
+
+
+    public void displayUserString(String userMessage) {
+        System.out.println(userMessage);
+    }
 }
