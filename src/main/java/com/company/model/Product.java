5fee7f9f28eda56249f701870b906c10ffb353ff
Bartosz Maleta <bartosz.maleta@gmail.com>
2020-09-11 01:49:16 +0200

    Add Product enum implementation

--- src/main/java/com/company/model/Product.java
+++ src/main/java/com/company/model/Product.java
@@ -1,5 +1,28 @@
 package com.company.model;
 
 public enum Product {
+    CHIPS(.50),
+    COLA(1.00),
+    CANDY(.65);
+
+    private int productInventory = 10;
+
+    private final double productCost;
+    Product(double productCost){
+        this.productCost = productCost;
+    }
+
+
+    public double getProductCost() {
+        return productCost;
+    }
+
+    public int getProductInventory() {
+        return productInventory;
+    }
+
+    public void setProductInventory(int prodInvt){
+        this.productInventory = prodInvt;
+    }
 
 }
